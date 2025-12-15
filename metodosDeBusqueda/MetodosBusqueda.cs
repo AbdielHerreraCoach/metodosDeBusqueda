ea71c4b781ef3f35a249b86348a8719a666f42c1
Lenovo <Lenovo@LENOVO>
2025-12-14 22:17:35 -0600

    Corrección de programas de búsqueda y ordenamiento

---
+++ metodosDeBusqueda/MetodosBusqueda.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MetodosBusqueda
+{
+    public static int[] GenerarArreglo(int tamaño)
+    {
+        Random random = new Random();
+        int[] arreglo = new int[tamaño];
+
+        for (int i = 0; i < tamaño; i++)
+            arreglo[i] = random.Next(1, 100);
+
+        return arreglo;
+    }
+
+    public static int BusquedaSecuencial(int[] arreglo, int valor)
+    {
+        for (int i = 0; i < arreglo.Length; i++)
+            if (arreglo[i] == valor)
+                return i;
+
+        return -1;
+    }
+
+    public static int BusquedaBinaria(int[] arreglo, int valor)
+    {
+        int inicio = 0;
+        int fin = arreglo.Length - 1;
+
+        while (inicio <= fin)
+        {
+            int medio = (inicio + fin) / 2;
+
+            if (arreglo[medio] == valor)
+                return medio;
+            else if (arreglo[medio] < valor)
+                inicio = medio + 1;
+            else
+                fin = medio - 1;
+        }
+
+        return -1;
+    }
+
+    public static Dictionary<int, bool> CrearTablaHash(int[] arreglo)
+    {
+        Dictionary<int, bool> tabla = new Dictionary<int, bool>();
+
+        foreach (int num in arreglo)
+            if (!tabla.ContainsKey(num))
+                tabla.Add(num, true);
+
+        return tabla;
+    }
+
+    public static bool BusquedaHash(Dictionary<int, bool> tabla, int valor)
+    {
+        return tabla.ContainsKey(valor);
+    }
+}
--- metodosDeBusqueda/Program.cs
+++ metodosDeBusqueda/Program.cs
@@ -1,57 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class MetodosBusqueda
+class Program
 {
-    // Búsqueda Secuencial
-    static int BusquedaSecuencial(int[] arreglo, int valor)
-    {
-        for (int i = 0; i < arreglo.Length; i++)
-        {
-            if (arreglo[i] == valor)
-                return i;
-        }
-        return -1;
-    }
-
-    // Búsqueda Binaria (arreglo ordenado)
-    static int BusquedaBinaria(int[] arreglo, int valor)
-    {
-        int inicio = 0;
-        int fin = arreglo.Length - 1;
-
-        while (inicio <= fin)
-        {
-            int medio = (inicio + fin) / 2;
-
-            if (arreglo[medio] == valor)
-                return medio;
-            else if (arreglo[medio] < valor)
-                inicio = medio + 1;
-            else
-                fin = medio - 1;
-        }
-        return -1;
-    }
-
-    // Búsqueda Hash
-    static bool BusquedaHash(Dictionary<int, bool> tabla, int valor)
-    {
-        return tabla.ContainsKey(valor);
-    }
-
     static void Main()
     {
-        Random random = new Random();
-        int[] arreglo = new int[10];
-
-        for (int i = 0; i < arreglo.Length; i++)
-            arreglo[i] = random.Next(1, 100);
-
-        Dictionary<int, bool> tablaHash = new Dictionary<int, bool>();
-        foreach (int num in arreglo)
-            if (!tablaHash.ContainsKey(num))
-                tablaHash.Add(num, true);
+        int[] arreglo = MetodosBusqueda.GenerarArreglo(10);
 
         Console.WriteLine("Arreglo generado:");
         Console.WriteLine(string.Join(", ", arreglo));
@@ -71,34 +25,37 @@ class MetodosBusqueda
         switch (opcion)
         {
             case 1:
-                resultado = BusquedaSecuencial(arreglo, valor);
-                if (resultado != -1)
-                    Console.WriteLine("Elemento encontrado en la posición " + resultado);
-                else
-                    Console.WriteLine("Elemento no encontrado");
+                resultado = MetodosBusqueda.BusquedaSecuencial(arreglo, valor);
+                MostrarResultado(resultado);
                 break;
 
             case 2:
                 Array.Sort(arreglo);
-                Console.WriteLine("Arreglo ordenado:");
+                Console.WriteLine("\nArreglo ordenado:");
                 Console.WriteLine(string.Join(", ", arreglo));
-                resultado = BusquedaBinaria(arreglo, valor);
-                if (resultado != -1)
-                    Console.WriteLine("Elemento encontrado en la posición " + resultado);
-                else
-                    Console.WriteLine("Elemento no encontrado");
+                resultado = MetodosBusqueda.BusquedaBinaria(arreglo, valor);
+                MostrarResultado(resultado);
                 break;
 
             case 3:
-                if (BusquedaHash(tablaHash, valor))
-                    Console.WriteLine("Elemento encontrado en la tabla hash");
-                else
-                    Console.WriteLine("Elemento no encontrado");
+                Dictionary<int, bool> tabla = MetodosBusqueda.CrearTablaHash(arreglo);
+                bool encontrado = MetodosBusqueda.BusquedaHash(tabla, valor);
+                Console.WriteLine(encontrado
+                    ? "Elemento encontrado en la tabla hash"
+                    : "Elemento no encontrado");
                 break;
 
             default:
                 Console.WriteLine("Opción inválida");
                 break;
         }
+    }
+
+    static void MostrarResultado(int resultado)
+    {
+        if (resultado != -1)
+            Console.WriteLine("Elemento encontrado en la posición: " + resultado);
+        else
+            Console.WriteLine("Elemento no encontrado");
     }
 }
